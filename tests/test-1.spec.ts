a4b6e9b80e5d7ce9ef1778559acccb915b31e9bd
Serhii Kravchenko <serg.kravchenko89@gmail.com>
2025-05-02 20:42:31 +0300

    add_tests

---
+++ tests/test-1.spec.ts
@@ -0,0 +1,24 @@
+import { test, expect } from '@playwright/test';
+
+test('Check selector and href attribute', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  await expect(page.getByRole('link', { name: 'Playwright logo Playwright' })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Playwright logo Playwright' })).toHaveAttribute(
+    'href',
+    '/',
+  );
+});
+
+test('Assert text', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  await expect(page.locator('h1')).toContainText(
+    'Playwright enables reliable end-to-end testing for modern web apps.',
+  );
+});
+
+test('Check light-dark mod', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  await page.getByLabel('Switch between dark and light').click();
+  await expect(page.locator('html')).toHaveAttribute('data-theme', 'dark');
+});
